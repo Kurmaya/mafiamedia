80ee1ca50afe10f840312dd123729d3c5ce631b8
Kurmaya <102325773+Kurmaya@users.noreply.github.com>
2023-08-20 16:50:09 +0530

    Update FilmPass.js

--- modules/FilmPass.js
+++ modules/FilmPass.js
@@ -1,9 +1,9 @@
 import {
 	ShaderMaterial,
 	UniformsUtils
-} from './three.module.js';
-import { Pass, FullScreenQuad } from './Pass.js';
-import { FilmShader } from './FilmShader.js';
+} from './modules/three.module.js';
+import { Pass, FullScreenQuad } from './modules/Pass.js';
+import { FilmShader } from './modules/FilmShader.js';
 
 class FilmPass extends Pass {
 
